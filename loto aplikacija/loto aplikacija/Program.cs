5f62c1553ff29d8167b057470464697d7560cafb
mstricev <mstricev@foi.hr>
2015-04-01 10:51:04 +0200

    Izrađeno grafičko sučelje aplikacije

--- loto aplikacija/loto aplikacija/Program.cs
+++ loto aplikacija/loto aplikacija/Program.cs
@@ -16,7 +16,7 @@ namespace loto_aplikacija
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLoto());
         }
     }
 }
